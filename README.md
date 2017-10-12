0d29314e74dd64d8f68e5d480d00da4ac381b392
Raphael Sperlich <spe16360@spengergasse.at>
2017-10-12 10:54:30 +0200

    Add readme content and initialise project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 1718-POS-Sperlich
+
+Motivation
+
+As a project in the subject programming and software engineering i have started this project
+
+
+Please contact spe16360@spengergasse.at for more info 
---
+++ src/test/java/com/spengergasse/QuerydslexerciseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spengergasse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuerydslexerciseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
